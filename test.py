a10b04cdf95c59da81c647410cf247df1a8cc187
Monish Kanungo <114340159+MonishKanungo@users.noreply.github.com>
2025-03-27 23:57:32 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-GENAI_API_KEY = "AIzaSyB3kV_-Q3b2ftVsBUCN_OcprTdMtgN8weg"
-genai.configure(api_key=GENAI_API_KEY)
-
-model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("Test")
-print(response.text)
